2d8c86e4604d8faa7d4b2c60feb280806002a5dd
SangBeomLeeE <120555778+SangBeomLeeE@users.noreply.github.com>
2024-05-19 18:21:19 +0900

    Add files via upload

--- urls.py
+++ urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('video_feed/', views.video_feed, name='video_feed'),
+    path('video_feed', views.video_feed, name='video_feed'),
+    path('recycle_chart', views.recycle_chart, name='recycle_chart'),
 ]
--- views.py
+++ views.py
@@ -1,25 +1,15 @@
-# camera/views.py
-
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
 import cv2
-
-def index(request):
-    return render(request, 'camera/index.html')
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+RTSP_STREAM_URL = "rtsp://192.168.0.12:8080/h264_ulaw.sdp"  # 핸드폰 RTSP URL로 변경
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)  # 웹캠 인덱스 0 사용
+        self.video = cv2.VideoCapture(RTSP_STREAM_URL)
         if not self.video.isOpened():
-            raise Exception("Could not open video device")
-        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            raise RuntimeError('Could not start video capture.')
+        # 버퍼 비활성화
+        self.video.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
     def __del__(self):
         self.video.release()
@@ -27,10 +17,30 @@ class VideoCamera(object):
     def get_frame(self):
         success, image = self.video.read()
         if not success:
-            raise Exception("Could not read frame from video device")
+            raise RuntimeError('Could not read frame from video stream.')
+        # 프레임 크기 조정
+        image = cv2.resize(image, (640, 480))
         ret, jpeg = cv2.imencode('.jpg', image)
+        if not ret:
+            raise RuntimeError('Could not encode image to JPEG.')
         return jpeg.tobytes()
+
+def index(request):
+    return render(request, 'camera/index.html')
+
+def gen(camera):
+    while True:
+        try:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        except RuntimeError as e:
+            print(f"Error: {e}")
+            break
 
 def video_feed(request):
     return StreamingHttpResponse(gen(VideoCamera()),
                                  content_type='multipart/x-mixed-replace; boundary=frame')
+
+def recycle_chart(request):
+    return render(request, 'camera/recycle_chart.html')
